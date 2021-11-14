22d5a92ec70546201de83a1299d9bab50e6993d2
Dejan Gloginjic <gloginjic.ra241.2020@uns.ac.rs>
2021-11-14 15:24:53 +0100

    Implemented class Student

---
+++ src/entity/Student.java
@@ -0,0 +1,23 @@
+package entity;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+enum Status{B, S};
+
+public class Student {
+	private String surname;
+	private String name;
+	private LocalDate dateOfBirth;
+	private String residentialAddress;
+	private int telephoneNumber;
+	private String eMail;
+	private String indexNumber;
+	private int yearOfEnrollment;
+	private int currentYearOfStudy;
+	private Status status;
+	private double averageGrade;
+	private HashMap<String, Integer> passedExams;
+	private ArrayList<String> remainingExams;
+}
